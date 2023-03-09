4a0777644d0f89fd81a7d96884f1880a833a44d6
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2023-03-09 14:57:53 +0700

    fix (navbar): set base container on navbar

--- components/Navbar/index.vue
+++ components/Navbar/index.vue
@@ -1,25 +1,27 @@
 <template>
-  <nav class="flex bg-white justify-between fixed top-0 left-0 right-0 px-[80px] py-5 z-10">
-    <nuxt-link to="/" class="hover:cursor-pointer">
-      <img
-        src="@/assets/logo.svg"
-        alt="Logo Al Jabbar"
-        width="234"
-        height="48"
-      >
-    </nuxt-link>
-    <ul class="flex items-center gap-6 text-[#757575] text-base">
-      <li
-        v-for="(menu, index) in navigationMenus"
-        :key="index"
-      >
-        <NavbarItem
-          :menu="menu"
-          :show-children="index === currentIndex"
-          @click="toggle(menu, index)"
-        />
-      </li>
-    </ul>
+  <nav class="bg-white fixed top-0 left-0 right-0 px-[80px] py-5 z-10">
+    <BaseContainer class="flex justify-between">
+      <NuxtLink to="/" class="hover:cursor-pointer">
+        <img
+          src="@/assets/logo.svg"
+          alt="Logo Al Jabbar"
+          width="234"
+          height="48"
+        >
+      </NuxtLink>
+      <ul class="flex items-center gap-6 text-[#757575] text-base">
+        <li
+          v-for="(menu, index) in navigationMenus"
+          :key="index"
+        >
+          <NavbarItem
+            :menu="menu"
+            :show-children="index === currentIndex"
+            @click="toggle(menu, index)"
+          />
+        </li>
+      </ul>
+    </BaseContainer>
   </nav>
 </template>
 
