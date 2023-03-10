7fe45f2b2d52a3441d3bdac3682760eb6aa5c97e
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2023-03-10 18:05:15 +0700

    Feat/hero banner (#11)

---
+++ components/Hero/Image/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="h-[740px]">
+    <div
+      class="w-full h-full absolute top-0"
+    />
+    <Carousel :items="images">
+      <template #filter>
+        <div
+          class="w-full h-full absolute top-0"
+        />
+      </template>
+    </Carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    images: {
+      type: Array,
+      default: () => ([])
+    }
+  }
+}
+</script>
