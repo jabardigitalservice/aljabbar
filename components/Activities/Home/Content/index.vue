27f72ddbb18177ae759062ccd2ab6c1cece4995b
yoslie <65396086+yoslie@users.noreply.github.com>
2023-03-14 08:51:37 +0700

    feat: add structure section activities in home page

---
+++ components/Activities/Home/Content/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="flex flex-col xl:flex-row">
+    <div class="flex flex-col">List</div>
+    <div class="flex justify-center">
+      <!-- TODO: replace with image -->
+      <div class="bg-gray-100 border border-gray-300 rounded-md w-[328px] h-[328px] md:w-[530px] md:h-[530px] xl:w-[515px] xl:h-[515px]" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
---
+++ components/Activities/Home/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <BaseContainer class="bg-white">
+    <div class="flex flex-col gap-6 md:gap-11 py-[60px] lg:py-24 xl:py-[120px]">
+      <BlogTitle
+        :title="`Agenda ${title}`"
+        :subtitle="subtitle"
+        :show-back-button="false"
+      >
+        <BlogSwitchTime />
+      </BlogTitle>
+      <ActivitiesHomeContent />
+    </div>
+  </BaseContainer>
+</template>
+
+<script>
+import { activities } from '~/static/data'
+
+export default {
+  name: 'ActivitiesHomeComponent',
+  data () {
+    const { title, subtitle } = activities
+    return {
+      title,
+      subtitle
+    }
+  },
+}
+</script>
